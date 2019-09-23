248cf74acd3910467daa19a33491c99c209303a4
WNJXYK <wnjxyk@gmail.com>
2019-09-23 15:30:52 +0800

    Update Main

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from Algorithm.GDFS import GDFS
 from Algorithm.FSFOA import FSFOA
 from Algorithm.IFSFOA import IFSFOA
 
-# # Test(Lasso, "Logs/Lasso.txt")
-# Test(GDFS, "Logs/GDFS.txt")
-# Test(FSFOA, "Logs/FSFOA.txt")
+Test(Lasso, "Logs/Lasso.txt")
+Test(GDFS, "Logs/GDFS.txt")
+Test(FSFOA, "Logs/FSFOA.txt")
 Test(IFSFOA, "Logs/IFSFOA.txt")
